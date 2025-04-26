064a8c462399677325da1a6dfc8d9106fbc3bdc0
Kuba Marchut <kuba.marchut@gmail.com>
2025-04-26 16:34:04 +0200

    adding simple example of reading packets from .pcap

---
+++ main.py
@@ -0,0 +1,16 @@
+from scapy.all import rdpcap
+
+
+def read_packets_from_file(filename):
+    packets = rdpcap(filename)
+
+    for i, packet in enumerate(packets[:5]):
+        print(f"Packet #{i+1}:")
+        print(packet.summary())
+        print(packet.show(dump=True))  # Pretty detailed view
+        print("=" * 50)
+
+
+if __name__ == "__main__":
+    filename = "pcaps/traffic.pcapng"
+    read_packets_from_file(filename)
